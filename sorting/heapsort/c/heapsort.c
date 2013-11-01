8fc4c893bdca69dfbe76e4a86cfd464821f7bff3
oxnz <yunxinyi@gmail.com>
2013-11-01 18:56:20 +0800

    update heapsort.c

--- sorting/heapsort/c/heapsort.c
+++ sorting/heapsort/c/heapsort.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "../../../utils/utils.h"
+
 void heap_adjust(int array[], int i, int size) {
     int lchild = 2*i;
     int rchild = 2*i+1;
@@ -31,28 +34,58 @@ void heap_adjust2(int array[], int i, int size) {
         else break;
     }
 }
-           
-void heap_sort(int array[], int n) {
-    int tmp;
-    for (int i = n/2-1; i >= 0; --i)
-        heap_adjust2(array, i, n);
-    for (int i = n-1; i > 0; --i) {
-        tmp = array[i];
-        array[i] = array[0];
-        array[0] = tmp;
-        heap_adjust2(array, 0, i);
+
+void heap_adjust3(int array[], int i, int size) {
+    for (int j = size/2; j >= 0; --j) {
+        if (2*j+1 < size) {
+            int index = array[2*j] > array[2*j+1] ? 2*j : 2*j+1;
+            if (array[j] < array[index])
+                swap(&array[j], &array[index]);
+        }
     }
 }
 
-#include <stdio.h>
+void sift_up(int a[], int i) {
+    for (;i > 1 && a[i] > a[i/2]; i = i/2)
+        swap(&a[i], &a[i/2]);
+}
+
+void sift_down(int a[], int i, int n) {
+    int j;
+    while (2*i < n) {
+        j = a[2*i] > a[2*i+1] ? 2*i : (2*i+1);
+        if (a[i] < a[j]) {
+            swap(&a[i], &a[j]);
+            i = j;
+        } else break;
+    }
+}
+
+void make_heap(int a[], int n) {
+    for (int i = n/2-1; i >= 0; --i)
+        sift_down(a, i, n);
+}
+
+void heap_sort(int array[], int n) {
+    int tmp;
+    //    for (int i = n/2-1; i >= 0; --i)
+    //  heap_adjust4(array, i, n);
+    make_heap(array, n);
+    printf("after adust\n");
+    print_array(array, n);
+    for (int i = n-1; i > 0; --i) {
+        swap(&array[i], &array[0]);
+        sift_down(array, 0, i-1);
+        //        heap_adjust4(array, 0, i);
+    }
+}
 
 int main(void) {
     int a[20] = {1, 2, 3, 9, 6, 5, 4, 32, 1231312, 54, 65, 76, 87, 220, 32};
+    random_array(a, 20);
+    print_array(a, 20);
     heap_sort(a, 20);
-    int i;
-    for (i = 0; i < 20; ++i)
-        printf("%d ", a[i]);
-    putchar('\n');
+    print_array(a, 20);
 
     return 0;
 }
