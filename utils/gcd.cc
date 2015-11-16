65e69d1512ffad49cfaf82e0392ca528c5084d1d
云心逸 <yunxinyi+github@gmail.com>
2015-11-16 13:59:17 +0800

    add tail-call elimination

--- utils/gcd.cc
+++ utils/gcd.cc
@@ -4,8 +4,20 @@ int gcd(int p, int q) {
     return gcd(q, p%q);
 }
 
+// tail-call elimination
+int gcd2(int p, int q) {
+	int t;
+	while (q) {
+		t = p;
+		p = q;
+		q = t % q;
+	}
+	return p;
+}
+
 #include <iostream>
 int main(void) {
     std::cout << "gcd(20, 30)=" << gcd(20, 30) << std::endl;
+    std::cout << "gcd(20, 30)=" << gcd2(20, 30) << std::endl;
     return 0;
 }
